388bae443766dea9951bc865bcdb850c88d785af
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-04-13 21:05:37 +0000

    modify

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,25 +3,27 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
-	public static void q1() {
-		//Write question 1 code here
+	public static int searchMazeMoves(String [][] arr){
+		int row=arr.length-1;
+		int col=0;
+		int noOfMoves=0;
+		return searchMazeHelper(row, col, noOfMoves, arr);
 	}
+	public static int searchMazeHelper(int row, int col, int moves, String[][] arr){
+		if(row<0||row>arr.length-1 || col>arr[row].length-1 ||col<0|| arr[row][col].equals("*")){
+			return -1;
+		}
+		if(arr[row][col].equals("F")){
+			return moves;
+		}
+		arr[row][col]= "*";
+		int up = searchMazeHelper(row-1, col, moves+1, arr);
+		int down = searchMazeHelper(row+1, col, moves+1, arr);
+		int right = searchMazeHelper(row, col+1, moves+1, arr);
+		int left = searchMazeHelper(row-1, col-1, moves+1, arr);
+		
+		
 
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
 	}
 
 }
